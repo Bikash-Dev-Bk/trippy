aaaff1e6d3ab39894d040033177184c390a33cc4
bikash-dev <bikash.dev.bk@gmail.com>
2023-04-07 11:51:03 +0600

    Banner

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react";
+import './Banner.css';
+import Button from "../../../components/Button/Button";
+import { Link } from "react-router-dom";
 
 const Banner = () => {
-    return (
-        <div>
-            <h1>Banner</h1>
+  return (
+    <div
+      className="hero min-h-screen banner-container"
+    >
+      <div className="hero-overlay bg-opacity-40"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-3xl">
+          <h1 className="mb-5 text-5xl font-bold">Enjoy Vacation With V-Travel</h1>
+          <p className="mb-5">
+          Travel to the any corner of the world, without going around in circles.
+          </p>
+          <Link to={'/booking'}><Button>Book A Tour</Button></Link>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
